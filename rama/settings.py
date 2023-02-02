1ebcca9fb68229cd696b95c26172347884803612
poncemarco <maarco.app98@gmail.com>
2023-02-01 21:02:56 -0600

    database configured if debug = False

--- rama/settings.py
+++ rama/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@9!nqfzk09q#ine1h5bnoiai$qo*b##z20u1&#lzrgf1@v)*!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['147.182.239.73']
+ALLOWED_HOSTS = ['147.182.239.73', 'localhost']
 
 
 # Application definition
@@ -74,14 +74,24 @@ WSGI_APPLICATION = 'rama.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': '315216976mP',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

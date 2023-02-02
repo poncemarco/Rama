9cf80d7a14bc845b15fe460cbc0effd191f43936
poncemarco <maarco.app98@gmail.com>
2023-02-02 08:15:10 -0600

    change the path to templates and static

--- rama/settings.py
+++ rama/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/static/assets/img/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
